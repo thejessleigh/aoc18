47baa54df21c8415e485056f09d875a6e23f03e9
thejessleigh <j.l.unrein@gmail.com>
2018-12-04 08:56:34 -0600

    refactor 3_1 solution & benchmark.

--- day3/3_1.py
+++ day3/3_1.py
@@ -1,39 +1,23 @@
 import re
 
+
 def claims(input):
-    c = input.read().splitlines()
     side = 1000
-    matrix = [["O" for x in range(side)] for y in range (side)]
-    idx = r'\d*(,)\d*'
-    dim = r'\d*(x)\d*'
-    cl = r'(#)\d*'
-    overlap = 0
-    for claim in c:
-        claimant = int(re.search(cl, claim).group(0)[1:])
-        print(claimant)
-        inidicies = [int(i) for i in re.search(idx, claim).group(0).split(',')]
-        print(inidicies)
-        dimensions = [int(i) for i in re.search(dim, claim).group(0).split('x')]
-        print(dimensions)
-        x = inidicies[0]
-        y = inidicies[1]
-        width = dimensions[0]
-        height = dimensions[1]
-        for _ in range(height):
-            for _ in range(width):
-                space = matrix[y][x]
-                if space == "O":
-                    matrix[y][x] = claimant
+    matrix = [["O" for x in range(side)] for y in range(side)]
+
+    pattern = re.compile(r'#(?P<claim_id>\d*)\s@\s(?P<x>\d*),(?P<y>\d*):\s(?P<w>\d*)x(?P<h>\d*)')
+    for claim in input.read().splitlines():
+        claim_details = dict((k, int(v)) for k, v in pattern.search(claim).groupdict().items())
+        y = claim_details['y']
+        for _ in range(claim_details['h']):
+            x = claim_details['x']
+            for _ in range(claim_details['w']):
+                if matrix[y][x] == "O":
+                    matrix[y][x] = claim_details['claim_id']
                 else:
                     matrix[y][x] = "X"
                 x += 1
-            x = inidicies[0]
             y += 1
-        check_overlap = 0
-    for x in matrix:
-        check_overlap += x.count("X")
-    print(check_overlap)
+    return sum(x.count("X") for x in matrix)
 
-    return overlap
-
-print(claims(open('input.txt', 'r')))
+claims(open('input.txt', 'r'))
---
+++ day3/benchmark.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "\`python3 3_1.py\` 100 times"
+time for i in {1..100}; do python3 3_1.py; done
